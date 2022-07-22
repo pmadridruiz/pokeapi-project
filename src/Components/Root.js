3345874837f7c680700dc7187823d5ad67603d19
Patricio Madrid Ruiz <pmadridruiz@gmail.com>
2022-07-22 00:26:22 -0400

    componentes re-creados, componentes de tailwind agregados

--- src/Components/Root.js
+++ src/Components/Root.js
@@ -1,28 +1,14 @@
-// APP BASE
-import { Outlet, Link } from "react-router-dom";
+import '.././index.css'
+import Search from './Search/Search';
 
 function Root() {
 
     return (
         <>
-            <h1 className='bg-red-400 text-center m-4 text-white font-bold'>Pókemon App</h1>
-            <nav className='border-b pb-4 bg-blue-600 font-bold text-center text-white'>
-                <Link to="/">Inicio</Link> |{" "}
-                <Link to="/buscar">Buscar Pókemon</Link> |{" "}
-                <Link to="/batalla">Batalla Pókemon</Link> |{" "}
-                <Link to="/todos">Pókemones</Link> |{" "}
-                <Link to="/pokedex">Pokédex</Link>
-
-            </nav>
-            <Outlet />
+            <Search />
         </>
     );
 }
 
 export default Root;
 
-
-// style={{
-//     borderBottom: "solid 1px",
-//     paddingBottom: "1rem",
-// }}
--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import Root from './Components/Root';
-import Combat from './Components/Combat/Combat';
-import Finder from './Components/Finder/Finder';
-import List from './Components/List/List';
-import Pokedex from './Components/Pokedex/Pokedex';
+// import Combat from './Components/Combat/Combat';
+// import List from './Components/List/List';
+// import Pokedex from './Components/Pokedex/Pokedex';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <Routes>
+    <Root />
+    {/* <Routes>
       <Route path="/" element={<Root />}>
         <Route path="buscar" element={<Finder />} />
         <Route path="batalla" element={<Combat />} />
         <Route path="todos" element={<List />} />
-        <Route path="pokedex/" element={<Pokedex />} />
+        <Route path="pokedex" element={<Pokedex />} />
       </Route>
-    </Routes>
-  </BrowserRouter>
+    </Routes > */}
+  </BrowserRouter >
   // </React.StrictMode>
 );
 
