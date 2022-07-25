67bb81bc1d48490f8851740c15c684f9cf9e229c
Patricio Madrid Ruiz <pmadridruiz@gmail.com>
2022-07-25 06:39:14 -0400

    cambio icono pokeball ventana

--- src/Components/Batalla/Batalla.js
+++ src/Components/Batalla/Batalla.js
@@ -29,34 +29,36 @@ function Batalla() {
     }
 
     return (
-        <>
+        <div className="bg-[url('../../Assets/pokeArena.png')] bg-cover">
 
-            <div className="md:flex flex grid-cols-2 gap-10 content-center h-screen justify-center">
+            <div className="md:flex flex grid-cols-2 gap-10 content-center h-screen justify-center ">
+                <div className='md:flex'>
+                    <div className='md:w-64 m-2 '>
+                        <h1 className='text-center'>Pokemon [1]</h1>
+                        <Search getPokemon={getPokemon} />
+                        {!cargando && pokemon ? (
+                            <div className='grid text-center justify-center'>
+                                <h1 className='font-bold capitalize pt-2'>{pokemon.name}</h1>
+                                <img src={pokemon?.sprites.front_default} alt={pokemon.name} />
+                                <h1>{pokemon?.stats[0].base_stat}</h1>
+                            </div>) : null}
+                    </div>
 
-                <div className='md:w-64 m-2'>
-                    <h1 className='text-center'>Pokemon [1]</h1>
-                    <Search getPokemon={getPokemon} />
-                    {!cargando && pokemon ? (
-                        <div className='grid text-center justify-center'>
-                            <h1 className='font-bold capitalize pt-2'>{pokemon.name}</h1>
-                            <img src={pokemon?.sprites.front_default} alt={pokemon.name} />
-                            <h1>{pokemon?.stats[0].base_stat}</h1>
-                        </div>) : null}
+                    <div className='md:w-64 m-2'>
+                        <h1 className='text-center'>Pokemon [2]</h1>
+                        <SearchDos getPokemonDos={getPokemonDos} />
+                        {!cargandoDos && pokemonDos ? (
+                            <div className='grid text-center justify-center'>
+                                <h1 className='font-bold capitalize pt-2'>{pokemonDos.name}</h1>
+                                <img src={pokemonDos?.sprites.front_default} alt={pokemonDos.name} />
+                                <h1>{pokemonDos?.stats[0].base_stat}</h1>
+                            </div>) : null}
+                    </div>
                 </div>
 
-                <div className='md:w-64 m-2'>
-                    <h1 className='text-center'>Pokemon [2]</h1>
-                    <SearchDos getPokemonDos={getPokemonDos} />
-                    {!cargandoDos && pokemonDos ? (
-                        <div className='grid text-center justify-center'>
-                            <h1 className='font-bold capitalize pt-2'>{pokemonDos.name}</h1>
-                            <img src={pokemonDos?.sprites.front_default} alt={pokemonDos.name} />
-                            <h1>{pokemonDos?.stats[0].base_stat}</h1>
-                        </div>) : null}
-                </div>
             </div>
 
-        </>
+        </div >
     )
 }
 
