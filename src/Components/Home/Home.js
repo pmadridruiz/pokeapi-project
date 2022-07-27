d8227697c613edc1df1d425640bbd17eec9b6582
Patricio Madrid Ruiz <pmadridruiz@gmail.com>
2022-07-27 05:02:40 -0400

    más cambios css

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 import '../Home/Home.css'
 import videoHome from '../../Assets/pokeOpening.mp4'
 
 function Home() {
     return (
         <div className="bg-[url('../../Assets/pokeHome.png')] bg-[length:100%_100%]">
-
             <div className="md:flex content-center justify-center ">
                 <div className='md:flex h-screen'>
                     <div className='md:w-96 m-4 pt-6'>
-                        <video className='border-red-400 border-4' src={videoHome} autoPlay loop />
-                        <h1 className='font-bold'>ATRÁPALOS YA</h1>
+                        <video className='border-cyan-900 border-4' src={videoHome} autoPlay loop />
+                        <Link to='/lista'>
+                            <div className='pt-4'>
+                                <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">ATRÁPALOS YA!</button>
+                            </div>
+                        </Link>
                     </div>
                 </div>
             </div>
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -31,7 +31,7 @@ function Search(props) {
                     <div className="flex pt-2 absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                         <svg aria-hidden="true" className="w-5 h-5 text-black dark:text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     </div>
-                    <input onSubmit={handleKeyDown} type="text" id="default-search" className="block p-4 pl-10 w-full text-sm bg-gray-50 rounded-lg border focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-300 dark:border-gray-600 dark:placeholder-black text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar Pókemon..." required />
+                    <input onSubmit={handleKeyDown} type="text" id="default-search" className="block p-4 pl-10 w-full text-sm bg-gray-200 rounded-lg border focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-300 dark:border-gray-600 dark:placeholder-black text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar Pókemon..." required />
                     <button onClick={handleOnClick} type="submit" className="block text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buscar</button>
                 </div>
             </form>
