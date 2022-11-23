88680868d0e5a28c2de8a018f014a7520f9bde46
Patricio Madrid Ruiz <pmadridruiz@gmail.com>
2022-11-22 22:49:32 -0300

    fixes heroku

--- src/Components/Tools/Logout.js
+++ src/Components/Tools/Logout.js
@@ -1,21 +1,17 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import { GlobalContext } from '../../Context/GlobalState'
+
 
 const LogoutButton = () => {
 
-    const { favoritos } = useContext(GlobalContext)
+
     const uriLogout = 'https://pokedexapp0213.herokuapp.com/inicio'
     const { logout } = useAuth0();
 
-    function clearLogout() {
-        logout({ returnTo: uriLogout })
-        localStorage.clear(favoritos);
-    }
 
     return (
 
-        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={clearLogout()}> Logout</button >
+        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={() => logout({ returnTo: uriLogout })}> Logout</button >
     )
 }
 
