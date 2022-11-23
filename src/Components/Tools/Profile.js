033cbb08c6c4b97fb2963f0220eab44dfc8a9d46
Patricio Madrid Ruiz <pmadridruiz@gmail.com>
2022-11-23 16:08:36 -0300

    more changes

--- src/Components/Tools/Profile.js
+++ src/Components/Tools/Profile.js
@@ -10,7 +10,7 @@ const Profile = () => {
 
     return (
         isAuthenticated && (
-            <div className='flex-col m-2'>
+            <div className='flex-col m-2 p-2'>
                 <h2>Hola, {user.name}{' '}</h2>
             </div>
         )
