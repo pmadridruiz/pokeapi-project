d44b96ca74209ab37b4f5347afe09e02a9a056bf
Patricio Madrid Ruiz <pmadridruiz@gmail.com>
2022-11-23 16:50:42 -0300

    more changes

--- src/Components/Tools/Login.js
+++ src/Components/Tools/Login.js
@@ -6,7 +6,7 @@ const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
     return (
-        <button type="button" onClick={() => loginWithRedirect()}>Ingresar</button>
+        <button className="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md bg-red-600 hover:bg-red-700 hover:shadow-lg focus:bg-red-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-600 active:shadow-lg transition duration-150 ease-in-out mb-1" type="button" onClick={() => loginWithRedirect()}>Ingresar</button>
     )
 }
 
