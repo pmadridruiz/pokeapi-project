88c061a05cc13518d3660ce4bbc6e1d939bd2a10
Patricio Madrid Ruiz <pmadridruiz@gmail.com>
2022-11-30 16:49:57 -0300

    major changes

--- src/Components/Tools/Login.js
+++ src/Components/Tools/Login.js
@@ -5,8 +5,13 @@ const LoginButton = () => {
 
     const { loginWithRedirect } = useAuth0();
 
+    function LoginClear() {
+        localStorage.clear()
+        loginWithRedirect()
+    }
+
     return (
-        <button className="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-0 focus:outline-none focus:ring-[#4285F4]/50 active:shadow-lg transition duration-150 ease-in-out mb-1" type="button" onClick={() => loginWithRedirect()}>Ingresar</button>
+        <button className="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-0 focus:outline-none focus:ring-[#4285F4]/50 active:shadow-lg transition duration-150 ease-in-out mb-1" type="button" onClick={LoginClear}>Ingresar</button>
     )
 }
 
